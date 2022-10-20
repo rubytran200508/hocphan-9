faf135c99e64c73af66f37904e1af6f3d5606cbc
rubytran200508 <rubytran200508@gmail.com>
2022-10-20 08:32:43 -0400

    ls6

---
+++ ls6/ls6project/sinhvien/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Name(models.Model):
+    Ten_hs = models.CharField(max_length=250)
---
+++ ls6/ls6project/sinhvien/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from . models import Name
+# Create your views here.
+# def index(request):
+#     response= HttpResponse()
+#     response.write("<h1>Hello world- Welcome </h1>")
+#     response.write("This is a student app abcd")
+#     return response
+def index(request):
+    Myname=Name.objects.all().values()
+    template = loader.get_template('sinhvien/base.html')
+    context = {
+        'Myname': 'Trần Vĩnh Bảo Ngọc'
+    }
+    return HttpResponse(template.render(context, request))
+def add(request):
+    template= loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+def addrecord(request):
+    y=request.POST('Ten_hs')
+    sinhvien=Name(Ten_hs=y)
+def delete (request, id):
+    sinhvien=Name.objects.get(id=id)
+    sinhvien=delete()
+    return HttpResponseRedirect(reverse('index'))
+
